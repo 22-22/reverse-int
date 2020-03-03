2ee29539b97c673b5fa7c1d5da41919ec138245c
Yuliya Kogol <kogol.julia@gmail.com>
2020-03-03 14:15:58 +0300

    feat: add function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function reverse (n) {
+        
+        //return the absolute value of a number
+        let nPositive = Math.abs(n);
+
+        let str = String(nPositive);
+
+        let arr = str.split('');
+
+        //if the last item of the array is 0, remove it
+        if (arr[arr.length - 1] == 0) arr.pop();
+        
+        let strReversed = arr.reverse().join('');
+
+        let result = +strReversed;
+        
+        return result;
   
 }
